23d56e6d87a94a0bd9321c5b4771abab8e55907f
UT01397 <UT01397@UNICOMTIC8>
2024-10-11 14:23:48 +0530

    Add project files.

---
+++ BookRentalManagementSystem_V1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalManagementSystem_V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            book PONNIYIN = new book("PONNIYIN SELVAN ", "PONNIYIN SELVAN ", "kalki", 10);
+
+
+        }
+    }
+
+  
+
+
+
+   
+}
---
+++ BookRentalManagementSystem_V1/book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalManagementSystem_V1
+{
+    internal class book
+    {
+        public book(string bookId, string title, string author, decimal rentalprice)
+        {
+            this.bookId = bookId;
+            this.title = title;
+            this.author = author;
+            this.rentalprice = rentalprice;
+        }
+
+        public override string ToString()
+        {
+            return $"{bookId} {title} {author} {rentalprice}";
+        }
+
+        private string bookId {  get; set; }
+        private string title { get; set; }
+        private string author { get; set; }
+        private decimal rentalprice { get; set; }
+
+    }
+
+    
+
+
+
+
+}
